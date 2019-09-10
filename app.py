dc9a22537c942847e9f97c3a299b6893d89c8bd9
= <zainr7989@gmail.com>
2019-09-09 18:03:52 -0700

    Add templates using Jinga.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from random import choice, sample
 
 app = Flask(__name__)
@@ -21,30 +21,7 @@ horoscopes = [
 @app.route('/')
 def index():
     """Show the homepage and ask the user's name."""
-    return """
-    <form action='/horoscope'>
-        <p>
-            What is your name?
-            <input type="text" name="name"/>
-        </p>
-        <p>
-            <input type="checkbox" name="show_horoscopes"/>
-            Show Horoscopes
-        </p>
-        <p>
-            How many horoscopes do you want?
-            <select name="num_horoscopes">
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-                <option value="4">Four</option>
-                <option value="5">Five</option>
-                <option value="6">Six</option>
-            </select>
-        </p>
-        <input type="submit">
-    </form>
-    """
+    return render_template('index.html')
 
 
 @app.route('/horoscope')
@@ -53,13 +30,21 @@ def get_horoscope():
     name = request.args.get('name')
     num_horoscopes = int(request.args.get('num_horoscopes'))
     show_horoscopes = request.args.get('show_horoscopes')
-    predictions = ', '.join(sample(horoscopes, num_horoscopes))
+    horoscopes_to_show = sample(horoscopes, num_horoscopes)
+    # predictions = ', '.join(sample(horoscopes, num_horoscopes))
 
+    return render_template(
+        'horoscopes.html',
+        name=name,
+        show_horoscopes=show_horoscopes,
+        horoscopes_to_show=horoscopes_to_show))
+
+"""
     if show_horoscopes:
         return f"Hello there, {name}: {predictions}."
     else:
         return f"Hello there, {name}! Have a nice day!"
-
+"""
 
 if __name__ == "__main__":
     app.run(debug=True)
